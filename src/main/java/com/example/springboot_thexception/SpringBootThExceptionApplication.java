bb51b9cce9bccaa93961acafdb74eed0615f3877
binhnguyen95 <invalidmyarse@gmail.com>
2022-09-28 10:48:23 +0700

    done

---
+++ src/main/java/com/example/springboot_thexception/SpringBootThExceptionApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_thexception;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootThExceptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootThExceptionApplication.class, args);
+	}
+
+}
